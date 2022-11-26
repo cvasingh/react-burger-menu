0b18e333fb9aff8b2c8d879a1b43a2a8b020ac8c
Shiva Singh <shivasingh.200087@gmail.com>
2022-11-26 16:44:23 +0530

    add appstore link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { slide as Menu } from 'react-burger-menu'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<div style={{ width: '100%', height: '100vh', backgroundColor: 'rgba(0, 250, 0,0.1)' }}>
+    <Menu right isOpen={true} />
+    <div>
+      <div>Home Page</div>
     </div>
-  );
+  </div>);
 }
 
 export default App;
